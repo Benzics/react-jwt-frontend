1d095fb5ed4a10ab9541a66758779b58c3c3294e
benzics <benjaminnicholas29@gmail.com>
2023-02-05 21:33:34 +0100

    Added yup package to handle validations

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,1 +1,79 @@
-import { Button } from "bootstrap";
+import { Button, Form } from "react-bootstrap";
+import { useFormik } from "formik";
+import AuthService from "../services/auth.service";
+
+const Register = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+        },
+        onSubmit: (values) => {
+            if(AuthService.register(values)) {
+                console.log('registered');
+            } else {
+                console.log('not registered');
+            }
+        },
+    });
+
+
+    return (
+        <Form onSubmit={formik.handleSubmit}>
+            <Form.Group>
+                <Form.Label>First Name</Form.Label>
+                <Form.Control
+                    type="text"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.firstName}
+                    id="firstName"
+                    name="firstName"
+                />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                    type="text"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.lastName}
+                    id="lastName"
+                    name="lastName"
+                />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                    type="email"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.email}
+                    id="email"
+                    name="email"
+                />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    type="email"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.password}
+                    id="password"
+                    name="password"
+                />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">Register</Button>
+        </Form>
+    )
+}
+
+export default Register;
